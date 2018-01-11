2292d295032cbd64bdce533fe88e4869f6e318b3
lisirrx <lisirrx@gmail.com>
2018-01-11 19:53:47 +0800

    fix bugs

--- src/main/java/com/unicorn/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/unicorn/authserver/config/AuthorizationServerConfig.java
@@ -40,7 +40,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security
                 .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
+                .checkTokenAccess("permitAll()")
+                .allowFormAuthenticationForClients();
     }
 
     @Override
--- src/main/java/com/unicorn/authserver/config/ResourceServerConfig.java
+++ src/main/java/com/unicorn/authserver/config/ResourceServerConfig.java
@@ -16,14 +16,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/users").anonymous()
-                .antMatchers("/users").hasAuthority("query-demo")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated();
     }
 }
